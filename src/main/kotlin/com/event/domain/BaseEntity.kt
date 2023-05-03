3bebde1be3715a14ab74f2ce959b59743ab5f02d
DayoungChoii <young12430@gmail.com>
2023-05-04 00:37:16 +0900

    feat: BaseEntity 추가

---
+++ src/main/kotlin/com/event/domain/BaseEntity.kt
@@ -0,0 +1,23 @@
+package com.event.domain
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.persistence.*
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected var createdDate: LocalDateTime = LocalDateTime.MIN
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected var updatedDate: LocalDateTime = LocalDateTime.MIN
+
+}
