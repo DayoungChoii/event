1263894808b7aa7b8081f38a81e6fc431da1177b
DayoungChoii <young12430@gmail.com>
2023-05-22 00:15:07 +0900

    feat: 공통 ExceptionHandler추가

--- src/main/kotlin/com/event/handler/CustomExceptionHandler.kt
+++ src/main/kotlin/com/event/handler/CustomExceptionHandler.kt
@@ -1,15 +1,20 @@
 package com.event.handler
 
+import com.event.util.logger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import kotlin.math.log
 
 @RestControllerAdvice
 class CustomExceptionHandler {
 
+    val logger = logger()
     @ExceptionHandler
     fun handleIllegalStateException(ex: CustomException): ResponseEntity<ErrorMessage> {
+
+        logger.info(ex.message)
 
         val errorMessage = ErrorMessage(
             HttpStatus.INTERNAL_SERVER_ERROR,
